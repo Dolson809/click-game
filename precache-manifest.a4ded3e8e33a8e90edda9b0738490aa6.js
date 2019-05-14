2ac2208de8e462af3f1ea410540509201c305ee6
Daniel <sanjose1994@gmail.com>
2019-05-14 13:52:33 -0700

    Updates

---
+++ precache-manifest.a4ded3e8e33a8e90edda9b0738490aa6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d41888383e8feb0372d8eb945ba968",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2db2c4160a9ed98bebc5",
+    "url": "/static/css/main.363c575e.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "2db2c4160a9ed98bebc5",
+    "url": "/static/js/main.f05c5d59.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "442f5b334f0bada649a831798be32477",
+    "url": "/static/media/food-1.442f5b33.jpg"
+  },
+  {
+    "revision": "80768bdac10a5c90ef4bdb9fef3599b0",
+    "url": "/static/media/food-10.80768bda.jpg"
+  },
+  {
+    "revision": "8c9b50f29e638c230cd6e22c9cb1edd6",
+    "url": "/static/media/food-11.8c9b50f2.jpg"
+  },
+  {
+    "revision": "769cf0b4dd2c8accafb4a068b6698da9",
+    "url": "/static/media/food-3.769cf0b4.jpg"
+  },
+  {
+    "revision": "555d77e743c47f3218af239370b3e346",
+    "url": "/static/media/food-4.555d77e7.jpg"
+  },
+  {
+    "revision": "c4ede9a20ee08ca21caa41863d9ef99b",
+    "url": "/static/media/food-6.c4ede9a2.jpg"
+  },
+  {
+    "revision": "075d1fb06a46abdfeb88d9911fc63c48",
+    "url": "/static/media/food-7.075d1fb0.jpg"
+  },
+  {
+    "revision": "785ee3d9f138a13bb056168edab1284b",
+    "url": "/static/media/food-8.785ee3d9.jpg"
+  },
+  {
+    "revision": "4ad0810c8284d4743eb451c6b435cd60",
+    "url": "/static/media/food-9.4ad0810c.jpg"
+  }
+]);
